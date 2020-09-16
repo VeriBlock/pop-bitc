a506009ed612bcd845ceba262619c69c752cb97f
Mr-Leshiy <leshiy12345678@gmail.com>
2020-09-16 18:12:33 +0300

    add merkle calculcation, pop_service

--- src/vbk/pop_service.hpp
+++ src/vbk/pop_service.hpp
@@ -2,3 +2,28 @@
 // https://www.veriblock.org
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCASH_SRC_VBK_POP_SERVICE_HPP
+#define BITCASH_SRC_VBK_POP_SERVICE_HPP
+
+#include "veriblock/altintegration.hpp"
+#include "veriblock/storage/batch_adaptor.hpp"
+
+class CDBWrapper;
+class CScript;
+typedef int64_t CAmount;
+
+namespace VeriBlock {
+
+using BlockBytes = std::vector<uint8_t>;
+using PoPRewards = std::map<CScript, CAmount>;
+
+altintegration::Altintegration& GetPop();
+void SetPopConfig(const altintegration::Config& config);
+void SetPop(CDBWrapper& db);
+std::string toPrettyString(const altintegration::Altintegration& pop);
+
+} // namespace VeriBlock
+
+
+#endif
